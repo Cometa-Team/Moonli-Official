1037c6848a00ff9a1ccb6b49989a1826b8860ec7
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-07-26 16:37:21 +0200

    Update ping.js

--- commands/anticrash/ping.js
+++ commands/anticrash/ping.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: "ping",
 	description: "Пинг бота",
 	async execute(client, message, args) {
-		let embed = Discord.MessageEmbed()
+		let embed = new Discord.MessageEmbed()
 		.setTitle("Мой пинг")
 		.setDescription(`Мой пинг: ${client.ws.ping}ms`)
 		.setTimestamp()
