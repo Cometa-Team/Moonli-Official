aaa8f680b587937c5aaa97819512ddf079381bbb
skat-spec <saskan085@gmail.com>
2021-07-28 13:43:34 +0200

    Обнова

--- commands/anticrash/whitelist.js
+++ commands/anticrash/whitelist.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "whitelist",
     description: "Добавить в Белый Список, чтобы бот вас игнорировал вас",
     async execute(client, message, args) {
-        client.mongo.db('AntiCrash').collection('AntiCrash').updateOne({ id: message.author.id }, { $set: { whitelist: on }}, { upsert: true })
+        client.mongo.db('AntiCrash').collection('AntiCrash').updateOne({ id: message.author.id }, { $set: { whitelist: 'on' }}, { upsert: true })
         let embed = new Discord.MessageEmbed()
         .setTitle("Вайтлист")
         .setColor(0x00ff09)
