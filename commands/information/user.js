38b88c36b6c02195a64ea5f63fe0cf6161ac08f2
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-13 12:34:33 +0300

    Update user.js

--- commands/information/user.js
+++ commands/information/user.js
@@ -8,6 +8,6 @@ module.exports = {
         .setTitle("Информация о пользователе")
         .setColor(0x00ff09)
         .setDescription("Скоро...")
-        message.channel.send(embed)
+        channel.send(content: embed)
     }
 }
