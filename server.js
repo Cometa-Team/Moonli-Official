65db1de3876957d49ef6d30fc0d80be6d9cc3bc8
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-14 20:15:20 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -40,6 +40,49 @@ for (const folder of commandFolders) {
 
 client.on("messageCreate", message => {
   console.log('Эвент messageCreate запущен!')
+  if(message.author.bot) return;
+  
+  if (!message.content.startsWith(config.prefix)) return;
+      const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+      const commandName = args.shift().toLowerCase();
+      const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
+      if (!command) return;
+      if (command.args && !args.length) {
+        let reply = `Вы не правильно написали, ${message.author}!\n\n`;
+        if (command.usage) {
+          reply += `Правильное использование команды: \`${guild.prefix}${command.name} ${command.usage}\``;
+        }
+        return message.channel.send({ content: reply });
+      }
+      if(!configdev.includes(message.author.id) && command.admin == true) {
+        console.log(`${message.author.tag} пытался использовать admin команду!`);
+        return message.react('❌');
+      }
+      if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+      }
+      const now = Date.now();
+      const timestamps = cooldowns.get(command.name);
+      const cooldownAmount = (command.cooldown || 5) * 1000;
+      if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+        if (now < expirationTime) {
+          const timeLeft = (expirationTime - now) / 1000;
+          return message.channel.send({ content: `Пожалуйста, подожди еще ${timeLeft.toFixed(1)} секунд(ы) прежде чем использовать команду \`${command.name}\`` });
+        }
+      }
+      timestamps.set(message.author.id, now);
+      setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+      try {
+        command.execute(client, message, args, command);
+      } catch (error) {
+        console.error(error);
+        message.channel.send(new Discord.MessageEmbed()
+        .setTitle("Ошибка!")
+        .setDescription(`\`\`\`` + error + `\`\`\``)
+        .setColor(client.colors.error))
+        console.log('Ошибка у бота!');
+      }
 });
 
 client.login(config.token);
