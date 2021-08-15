c9213e134eaabf0cf14a342355f07665664b7451
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-15 22:06:18 +0300

    Create bot.js

---
+++ commands/information/bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+const { version } = require('../../package.json')
+const { version: discordjsVersion } = require('discord.js')
+const ms = require('pretty-ms');
+const { author } = require('../../package.json');
+
+module.exports = {
+  name: "bot",
+  category: "user",
+  async execute(client, message, args) {
+    let members = client.guilds.cache.members
+    let adm = client.users.cache.get("852984192421199923")
+    let user = message.author;
+  message.channel.send(
+      new Discord.MessageEmbed()
+      .setColor(0x00ff00)
+      .setTitle(`Версия бота: ${version}`)
+      .setDescription(`Автор бота: ${author} \nЕго тег ${adm.tag}`)
+      .addField('Бот в сети:', `${ms(client.uptime)}`,true)
+      .addField('Нагрузка на бота:', `${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB RSS\n ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB Heap`,true)
+      .addField('Пинг:', `${client.ws.ping} ms`,true)
+      .addField('Всего гильдий:', `${client.guilds.cache.size}`,true)
+      .addField('Всего юзеров:', `${client.users.cache.size}`,true)
+      .addField('Всего команд:', `${client.commands.size}`,true)
+      .addField('Node js:', `${process.version} на ${process.platform} ${process.arch}`,true)
+      .addField('Библиотека Djs', `${discordjsVersion}`,true)
+  )
+ }
+}
