f3a69f34df4e956df83018cf7a64fe9aeb9661df
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-16 10:46:37 +0300

    Update react-verify.js

--- react-verify.js
+++ react-verify.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 "use strict";
 
-const { Discord, Intents, Collection } = require('discord.js');
+const { Client, Discord, Intents, Collection } = require('discord.js');
 const chalk = require("chalk");
 const moment = require("moment");
 const { BOT_TOKEN, VERIFICATION_CHANNEL, VERIFIED_ROLE } = require("./config.json");
 
-const client = new Discord.Client({
+const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING],
   disableEveryone: true
 });
