9c5d8d5083de3f89e633108d898b3bfac5697f6a
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-16 10:41:03 +0300

    Update react-verify.js

--- react-verify.js
+++ react-verify.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 "use strict";
 
-const Discord = require("discord.js");
+const { Client, Intents, Collection } = require('discord.js');
 const chalk = require("chalk");
 const moment = require("moment");
 const { BOT_TOKEN, VERIFICATION_CHANNEL, VERIFIED_ROLE } = require("./config.json");
 
 const client = new Discord.Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING],
   disableEveryone: true
 });
 
@@ -21,30 +22,30 @@ client.on("message", message => {
     if (!message.channel.permissionsFor(message.guild.me).serialize().SEND_MESSAGES) return console.error("The bot doesn't have the permission to send messages.\nRequired permission: SEND_MESSAGES");
     if (!message.channel.permissionsFor(message.guild.me).serialize().ADD_REACTIONS) {
       console.error("The bot doesn't have the permission to add reactions.\nRequired permission: `ADD_REACTIONS`");
-      message.channel.send("The bot doesn't have the permission to add reactions.\nRequired permission: `ADD_REACTIONS`")
+      message.channel.send({ content: "У бота нет разрешения на добавление реакций. \ NТребуемое право: `ADD_REACTIONS`" })
         .then(m => m.delete({timeout: 20000}));
       return;
     }
     if (!message.channel.permissionsFor(message.guild.me).serialize().MANAGE_MESSAGES) {
       console.error("The bot doesn't have the permission to delete messages.\nRequired permission: `MANAGE_MESSAGES`");
-      message.channel.send("The bot doesn't have the permission to delete messages.\nRequired permission: `MANAGE_MESSAGES`")
+      message.channel.send({ content: "The bot doesn't have the permission to delete messages.\nRequired permission: `MANAGE_MESSAGES`" })
         .then(m => m.delete({timeout: 20000}));
       return;
     }
     const messageRole = message.guild.roles.cache.find(role => role.name === VERIFIED_ROLE);
     if (messageRole == null) return;
     if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
-      message.channel.send("The bot doesn't have the permission required to assign roles.\nRequired permission: `MANAGE_ROLES`")
+      message.channel.send({ content: "The bot doesn't have the permission required to assign roles.\nRequired permission: `MANAGE_ROLES`" })
         .then(m => m.delete({timeout: 20000}));
       return;
     }
     if (message.guild.me.roles.highest.comparePositionTo(messageRole) < 1) {
-      message.channel.send("The position of this role is higher than the bot's highest role, it cannot be assigned by the bot.")
+      message.channel.send({ content: "The position of this role is higher than the bot's highest role, it cannot be assigned by the bot." })
         .then(m => m.delete({timeout: 20000}));
       return;
     }
     if (messageRole.managed == true) {
-      message.channel.send("This is an auto managed role, it cannot be assigned.")
+      message.channel.send({ content: "This is an auto managed role, it cannot be assigned." })
         .then(m => m.delete({timeout: 20000}));
       return;
     }
