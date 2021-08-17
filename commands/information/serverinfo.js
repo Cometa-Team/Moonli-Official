43eb081b345179f32d7bd0298c891bab3f6fc90d
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-17 09:40:18 +0300

    Update serverinfo.js

--- commands/information/serverinfo.js
+++ commands/information/serverinfo.js
@@ -7,13 +7,13 @@ module.exports = {
         let news = message.guild.publicUpdatesChannelId || 'Нету'
         let embed = new MessageEmbed()
         .setTitle("Информация о сервере")
-        .setDescription(`Овнер: ${message.guild.ownerId}
-Айди: ${message.guild.id}
-Название: ${message.guild.name}
-Участников: ${message.guild.memberCount}
-Канал с новостями: ${news}
-Уровень сложности: ${message.guild.verificationLevel}
-Локация: ${message.guild.preferredLocale}`)
+        .setDescription(`👑Овнер: ${message.guild.ownerId}
+🆔Айди: ${message.guild.id}
+🚅Название: ${message.guild.name}
+👮Участников: ${message.guild.memberCount}
+📺Канал с новостями: ${news}
+🆙Уровень сложности: ${message.guild.verificationLevel}
+📍Локация: ${message.guild.preferredLocale}`)
         .setTimestamp()
         message.reply({ embeds: [embed] });
     },
