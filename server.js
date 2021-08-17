afdbcfb2be4aef8d667f1f3bf788ce7d47675252
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-17 20:30:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ client.on('interactionCreate', async interaction => {
 
 
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://vynixh:MINICAT2019@vynix.51j3t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://vynixh:vynixh1@vynix.51j3t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 client.mongo = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.mongo.connect(err => {
   const collection = client.mongo.db("Vynix").collection("VynixHelper");
