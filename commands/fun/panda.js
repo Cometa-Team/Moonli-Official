9ebd797163cb678269df7dd1bfadb5c306da3571
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-17 09:25:05 +0300

    Create panda.js

---
+++ commands/fun/panda.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js")
+const api = require("node-fetch")
+
+module.exports = {
+  name: 'panda',
+  category: 'fun',
+  async execute(client, message, args) {
+    let res = api("https://some-random-api.ml/img/panda")
+        .then(res => res.json())
+        .then(json => {
+          const embed = new MessageEmbed()
+          .setTitle("Панда")
+          .setColor(0x000000)
+          .setImage(json.link)
+          .setTimestamp()
+          message.channel.send({ embeds: [embed] })
+        })
+  }
+}
