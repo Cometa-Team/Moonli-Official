a5c2ef53658ffb817c94ed1157f00565672b8029
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-17 09:11:46 +0300

    Fun категория апдейт

--- commands/fun/dog.js
+++ commands/fun/dog.js
@@ -1,1 +1,19 @@
+const { MessageEmbed } = require("discord.js")
+const api = require("node-fetch")
 
+module.exports = {
+  name: 'dog',
+  category: 'fun',
+  async execute(client, message, args) {
+    let res = api("https://some-random-api.ml/img/dog")
+        .then(res => res.json())
+        .then(json => {
+          const embed = new MessageEmbed()
+          .setTitle("Собака")
+          .setColor(0x000000)
+          .setImage(json.link)
+          .setTimestamp()
+          message.channel.send({ embeds: [embed] })
+        })
+  }
+}
