412db0c11fb2bb7cb8201aa2e5635c85fad1163b
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-08-19 11:29:04 +0300

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -16,7 +16,7 @@ module.exports = {
       const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
       if (!command) return;
       let user = client.database.collection('Users').findOne({ id: message.author.id })
-      if(!user) { client.database.collection('Users').updateOne({ id: message.author.id }, { $set: { id: message.author.id, whitelist: 'off', badges: [], money: '0' }}, { upsert: true }); } 
+      if(!user) { client.database.collection('Users').updateOne({ id: message.author.id }, { $set: { id: message.author.id, guildid: message.guild.id, whitelist: 'off', badges: [], money: '0' }}, { upsert: true }); } 
       //if(message.channel.type === 'dm') {
         //return newGuildHook1.send(
           //new Discord.MessageEmbed()
