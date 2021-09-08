19b77e94ca75043afff4ef933cc9612d6439b6ab
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-09-08 08:32:16 +0300

    Update user.js

--- commands/information/user.js
+++ commands/information/user.js
@@ -4,10 +4,18 @@ module.exports = {
     name: "user",
     description: "Посмотреть свой или чжой профиль",
     async execute(client, message, args) {
+        let user = await User.findOne({ guildID: message.guild.id, userID: message.author.id });
         let embed = new MessageEmbed()
         .setTitle("Информация о пользователе")
         .setColor(0x00ff09)
-        .setDescription("Скоро...")
+        .setDescription(`Имя: message.author.username
+Айди: message.author.id
+Тег: message.author.discriminator
+Биткойны: user.bitcoin
+Дон. Биткойны: user.donbitcoin
+Премиум: user.premium
+Сообщения: user.messages
+`)
         message.reply({ embeds: [embed] });
     }
 }
