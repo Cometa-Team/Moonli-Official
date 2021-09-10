f50bd44ba6c6c33516151e967c4270ef3a93e094
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-09-10 11:24:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -59,9 +59,38 @@ mongoose.connection.on('connected',()=>{
   console.log('[✅ DataBase] Connected!')
 })
 
-client.on('interactionCreate', interaction => {
-	if (!interaction.isButton()) return;
-	console.log(interaction);
-});
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json');
+const fs = require('fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+// Place your client and guild ids here
+const clientId = '884764512014700554';
+const guildId = message.guild.id
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
 
 client.login(client.config.token)
