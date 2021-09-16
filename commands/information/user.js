5d331099d4ee1f6a9ea82f1ddef2e0be39248944
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-09-16 10:25:19 +0300

    Update user.js

--- commands/information/user.js
+++ commands/information/user.js
@@ -15,17 +15,12 @@ module.exports = {
         let date3 = new Date(message.guild.members.cache.get(argsUser.id).joinedTimestamp)
         let diff1 = Math.round(Math.abs((date1.getTime() - date2.getTime()) / day))
         let diff2 = Math.round(Math.abs((date1.getTime() - date3.getTime()) / day))
-        const botik = argsUser.bot || 'Пользователь'
-        let user = await User.findOne({ guildID: message.guild.id, userID: message.author.id });
+        
         let embed = new MessageEmbed()
         .setTitle("Информация о пользователе")
         .setColor(client.color)
         .setDescription(`Имя: ${argsUser.username}
 Айди: ${argsUser.id}
-Биткойны: ${user.bitcoin}
-Дон. Биткойны: ${user.donbitcoin}
-Премиум: ${user.premium}
-Сообщения: ${user.messages}
 Аккаунт создан: **${strftime('%d.%m.%Y в %H:%M', new Date(argsUser.createdTimestamp))}\n(${diff1} дн. назад)**
 Присоеденился к серверу: **${strftime('%d.%m.%Y в %H:%M', new Date(message.guild.member(argsUser).joinedTimestamp))}\n(${diff2} дн. назад)**
 `)
