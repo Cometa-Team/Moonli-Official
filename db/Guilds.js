77b6d1662dc2586367f91362377ba77a2cdfb048
skat-spec <66431558+skat-spec@users.noreply.github.com>
2021-09-20 20:34:50 +0300

    Delete Guilds.js

--- db/Guilds.js
+++
@@ -1,7 +0,0 @@
-const config = require('../config.json')
-const schema = mongoose.Schema({
-    guildID: String,
-    prefix: { type: String, default: config.prefix },
-    premium: { type: String, default: "нет" }
-});
-module.exports = mongoose.model("Guild", schema)
