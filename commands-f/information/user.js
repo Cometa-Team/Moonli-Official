2ade11827ec4fba93e5496239fffd894bb2e5dff
skat-spec <saskan085@gmail.com>
2021-10-03 09:37:09 +0300

    Release

--- commands-f/information/user.js
+++ commands-f/information/user.js
@@ -8,11 +8,11 @@ module.exports = {
     let user = message.mentions.users.first() || message.author
 
     let userm = new MessageEmbed()
-      .setTitle("**Userinfo**")
-      .setColor(client.color)
-      .setThumbnail(user.avatarURL())
-      .setDescription(`**Username**: ${user.username}\n**Tag**: #${user.discriminator}\n**Id**: ${user.id}\n**Status**: ${user.presence.status}\n**Bot**: ${user.bot}\n**createdAt**: ${user.createdAt}`)
-      .setTimestamp()
+    .setTitle("**Userinfo**")
+    .setColor(client.color)
+    .setThumbnail(user.avatarURL())
+    .setDescription(`**Username**: ${user.username}\n**Tag**: #${user.discriminator}\n**Id**: ${user.id}\n**Status**: ${user.presence.status}\n**Bot**: ${user.bot}\n**createdAt**: ${user.createdAt}`)
+    .setTimestamp()
 
     message.channel.send({ embeds: [userm] });
   }
