9de03683e5da55d8e4cd9eac6705f2241c041276
skat-spec <66431558+skat-spec@users.noreply.github.com>
2022-03-02 18:36:12 +0300

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -9,8 +9,12 @@ module.exports = {
     aliases: ['ebal', 'e'],
     category: "owner",
     async execute(client, message, args) {
-        if((message.content.includes("fs")) message.reply({ content: "Недоступно" })
-        if((message.content.includes("fs")) message.reply({ content: "Недоступно" })
+        if((message.content.includes("fs")) {
+            message.reply({ content: "Недоступно" })
+        }
+        if((message.content.includes("fs")) {
+            message.reply({ content: "Недоступно" })
+        }
       	const database = `Eval {
       	id: ${message.author.id},
       	username: ${message.author.username},
