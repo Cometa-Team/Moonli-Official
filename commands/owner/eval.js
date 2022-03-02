7679cd49908e2f939e285dd6e31451accb45b52e
skat-spec <66431558+skat-spec@users.noreply.github.com>
2022-03-02 20:09:21 +0300

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -43,7 +43,7 @@ module.exports = {
               return message.channel.send("Недоступно")
         }
         if (message.content.indexOf("this") > -1) {
-              message.channel.send("Недоступно")
+              return message.channel.send("Недоступно")
         }
       	const database = `Eval {
       	id: ${message.author.id},
