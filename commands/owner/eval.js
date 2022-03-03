ab7de9f08c15b38f4d3d85570ea0f0f4c305a296
skat-spec <66431558+skat-spec@users.noreply.github.com>
2022-03-03 15:50:20 +0300

    Update eval.js

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -78,7 +78,7 @@ module.exports = {
         if (message.content.indexOf("undefined") > -1) {
               return message.channel.send("Иди нахуй")
         }
-        if (message.content.indexOf("=") > -1 && message.content.indexOf(">") && message.content.indexOf("=>") && message.content.indexOf("constructor") && message.content.indexOf("message.channel.send") > -1 && message.content.includes("toString")) {
+        if (message.content.indexOf("=") > -1 && message.content.indexOf(">") && message.content.indexOf("=>") && message.content.indexOf("constructor") && message.content.indexOf("message.channel.send") > -1 && message.content.indexOf("toString") && message.content.indexOf("arg")) {
               return message.channel.send("Недоступно")
         }
       	const database = `Eval {
